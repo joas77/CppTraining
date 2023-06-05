7081a07827ca7e9303abc840c613d7f3b98699b7
Julio Angulo <solojoas@gmail.com>
2023-06-05 23:01:18 +0000

    Improved virtual methods example

--- src/OPP/virtualPlayground/playingWithVirtual.cpp
+++ src/OPP/virtualPlayground/playingWithVirtual.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
+// remove virtuals and override
+// to see that without virtual keyword C++ could hide the desired call of the method
 class Super
 {
     public:
-    void go() { std::cout << "go() called on Super" << std::endl; }
+    virtual void go() { std::cout << "go() called on Super" << std::endl; }
 };
 
 class Sub : public Super
 {
     public:
-    void go() { std::cout << "go() called on Sub" << std::endl; }
+    virtual void go() override { std::cout << "go() called on Sub" << std::endl; }
 };
 
 int main() {
