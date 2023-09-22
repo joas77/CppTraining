c2032c487b917956f4ac5faa904ea63e0bf6ee3b
Julio Angulo <solojoas@gmail.com>
2023-09-22 17:15:59 +0000

    Implemented basic methods of vector

--- src/templates/main.cpp
+++ src/templates/main.cpp
@@ -5,8 +5,17 @@ int main()
     Vector<double> vd(10);
     Vector<int> vi(10);
 
+    int i=0;
+    for (auto &e : vi)
+    {
+        e = ++i;
+    }
+    
+
     printVector(vd);
     printVector(vi);
+
+    std::cout << vi * vi << std::endl;
 
     return 0;
 }
--- src/templates/vector.h
+++ src/templates/vector.h
@@ -14,6 +14,9 @@ public:
     Vector& operator=(Vector &&);
     const T& operator[](std::size_t) const;
     T& operator[](std::size_t);
+
+    T operator*(const Vector&) const;
+
     std::size_t size() const;
     T* begin();
     T* end();
@@ -53,10 +56,41 @@ const T& Vector<T>::operator[](std::size_t index) const {
 }
 
 template <typename T>
-void printVector(const Vector<T>& v) {
-    for (std::size_t i = 0; i < v.size(); i++)
+T* Vector<T>::begin() {
+    return &data_[0];
+}
+
+template <typename T>
+T* Vector<T>::end() {
+    return &data_[kSize_];
+}
+
+template <typename T>
+const T* Vector<T>::begin() const {
+    return &data_[0];
+}
+
+template <typename T>
+const T* Vector<T>::end() const {
+    return &data_[kSize_];
+}
+
+template <typename T>
+T Vector<T>::operator*(const Vector& v) const {
+    // WARNING: for simplicity not checking bound limits
+    T sum = 0;
+    for (size_t i = 0; i < size(); i++)
     {
-        std::cout << v[i] << " ";
+        sum += data_[i] + v[i];
+    }
+    return sum;
+}
+
+template <typename T>
+void printVector(const Vector<T>& v) {
+    for (const auto& elem: v )
+    {
+        std::cout << elem << " ";
     }
     std::cout << std::endl;
 }
