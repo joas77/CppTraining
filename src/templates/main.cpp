54f13c2362c56839a62c6769946a615f0baeb81b
Julio Angulo <solojoas@gmail.com>
2023-09-25 15:39:35 +0000

    Finished templates examples

--- src/templates/main.cpp
+++ src/templates/main.cpp
@@ -1,21 +1,34 @@
 #include "vector.h"
+#include "sum.h"
 
 int main()
 {
     Vector<double> vd(10);
     Vector<int> vi(10);
 
-    int i=0;
+    int i = 0;
     for (auto &e : vi)
     {
         e = ++i;
     }
-    
 
     printVector(vd);
     printVector(vi);
 
-    std::cout << vi * vi << std::endl;
+    /*
+     *  u * v  ==> dot product between u, v
+     * e.g
+     * u = [ 1 2 3 ]
+     * v = [ 4 5 6]
+     * u * v = 1*4 + 2*5 + 3*6
+     */
+    std::cout << "vi . vi = " << vi * vi << std::endl;
+
+    // implicit
+    std::cout << " 3 + 5 = " << sum(3, 5) << std::endl;
+
+    // explicit
+    std::cout << "3.14 + 2.7 = " << sum<float>(3.14, 2.7) << std::endl;
 
     return 0;
 }
---
+++ src/templates/sum.h
@@ -0,0 +1,6 @@
+// function template
+template <typename T>
+T sum(T x,  T y)
+{
+    return x + y;
+}
